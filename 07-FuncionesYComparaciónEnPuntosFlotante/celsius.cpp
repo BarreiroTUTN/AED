f932167e823e1b87017e32a9b0bc2243260aa5b4
BarreiroT <tomasmbarreiroi@gmail.com>
2022-06-15 20:24:46 -0300

    especificación validateTolerance

--- 07-FuncionesYComparaciónEnPuntosFlotante/celsius.cpp
+++ 07-FuncionesYComparaciónEnPuntosFlotante/celsius.cpp
@@ -3,11 +3,11 @@
 double farenheitToCelsiusWithValue(double);      // f: R -> R / f(x) = (x - 32) * 0.55555555555;
 double farenheitToCelsiusWithExpression(double); // f: R -> R / f(x) = (x - 32) * 5 / 9;
 
-bool validateTolerance(double, double, double);
+bool validateTolerance(double, double, double); // f: R -> R / f(x, y, z) = x < y + z && x > y - z;
 
 int main()
 {
-    double tolerance = 0.0000001;
+    double tolerance = 0.000000001;
 
     double farenheitValues[] = {-25, -22.5, -20, -15, -10, -5, -1, 0, 32, 40, 42.5, 45, 47.5, 50, 52.5, 55, 60, 70, 80};
 
