cebb2e3442cff970cf1e3781d2c60fd3767818f9
BarreiroT <tomasmbarreiroi@gmail.com>
2022-06-29 22:47:25 -0300

    ...

--- 07-FuncionesYComparacionEnPuntosFlotante/distancia.cpp
+++ 07-FuncionesYComparacionEnPuntosFlotante/distancia.cpp
@@ -1,6 +1,8 @@
 #include <cassert>
 #include <cmath>
 
+using namespace std;
+
 bool AreNear(double, double, double = 0.001);
 // f: R x R x R^+ -> B / f(x, y, z) =
 // V si y - z <= x <= y + z;
@@ -11,57 +13,13 @@ struct Point
     double x{0}, y{0}, z{0};
 };
 
-double distanceBetween(double, double, double, double); // f:R^2xR^2--> R/f(ax,ay,bx,by) = ((x1-x2)^2 + (y1-y2)^2)^1/2
-double distanceBetweenRn(double, double, double = 0, double = 0, double = 0, double = 0);
 double distanceBetweenPoints(Point a, Point b);
 
-void testDistanceBetween();
-void testDistanceBetweenRn();
 void testDistanceBetweenPoints();
 
 int main()
 {
-    testDistanceBetween();
-    testDistanceBetweenRn();
     testDistanceBetweenPoints();
-}
-
-void testDistanceBetween()
-{
-    assert(AreNear(distanceBetween(5, 5, 0, 0), 7, 1));
-    assert(AreNear(distanceBetween(0, 0, 5, 5), 7, 1));
-
-    assert(AreNear(distanceBetween(5, 5, 5, 5), 0));
-    assert(AreNear(distanceBetween(5, 5, 5, 5), 0));
-
-    assert(AreNear(distanceBetween(4, 2, 5, 8), 6, 1));
-    assert(AreNear(distanceBetween(5, 8, 4, 2), 6, 1));
-
-    assert(AreNear(distanceBetween(-5, 0, 5, 0), 10));
-    assert(AreNear(distanceBetween(5, 0, -5, 0), 10));
-
-    assert(AreNear(distanceBetween(0, -5, 0, 5), 10));
-    assert(AreNear(distanceBetween(0, 5, 0, -5), 10));
-}
-
-void testDistanceBetweenRn()
-{
-    assert(AreNear(distanceBetweenRn(5, 5), 0));
-
-    assert(AreNear(distanceBetweenRn(5, -5), 10));
-    assert(AreNear(distanceBetweenRn(-5, 5), 10));
-
-    assert(AreNear(distanceBetweenRn(5, 0, 5, 0), 7, 1));
-    assert(AreNear(distanceBetweenRn(0, 5, 0, 5), 7, 1));
-
-    assert(AreNear(distanceBetweenRn(5, 5, 5, 5), 0));
-    assert(AreNear(distanceBetweenRn(5, 5, 5, 5), 0));
-
-    assert(AreNear(distanceBetweenRn(4, 5, 2, 8), 6, 1));
-    assert(AreNear(distanceBetweenRn(5, 4, 8, 2), 6, 1));
-
-    assert(AreNear(distanceBetweenRn(0, 0, 0, 0, -5.0, 5.0), 10));
-    assert(AreNear(distanceBetweenRn(0, 0, 0, 0, 5.0, -5.0), 10));
 }
 
 void testDistanceBetweenPoints()
@@ -74,26 +32,11 @@ void testDistanceBetweenPoints()
     assert(AreNear(distanceBetweenPoints(Point{5, 5}, Point{5, 5}), 0));
     assert(AreNear(distanceBetweenPoints(Point{5, 5}, Point{5, 5}), 0));
 
-    assert(AreNear(distanceBetweenPoints(Point{0, 0, -5}, Point{0, 0, 5}), 6, 1));
-    assert(AreNear(distanceBetweenPoints(Point{0, 0, 5}, Point{0, 0, -5}), 6, 1));
-}
+    assert(AreNear(distanceBetweenPoints(Point{0, 5}, Point{0, -5}), 10));
+    assert(AreNear(distanceBetweenPoints(Point{0, -5}, Point{0, 5}), 10));
 
-double distanceBetween(double ax, double ay, double bx, double by)
-{
-    double distanceBetweenX = pow(ax - bx, 2);
-
-    double distanceBetweenY = pow(ay - by, 2);
-
-    return sqrt(distanceBetweenX + distanceBetweenY);
-}
-
-double distanceBetweenRn(double ax, double bx, double ay, double by, double az, double bz)
-{
-    double distanceBetweenX = (ax - bx) * (ax - bx);
-
-    double distanceBetweenZ = pow(az - bz, 2);
-
-    return sqrt(distanceBetweenX + pow(ay - by, 2) + distanceBetweenZ);
+    assert(AreNear(distanceBetweenPoints(Point{0, 0, -5}, Point{0, 0, 5}), 10, 1));
+    assert(AreNear(distanceBetweenPoints(Point{0, 0, 5}, Point{0, 0, -5}), 10, 1));
 }
 
 double distanceBetweenPoints(Point a, Point b)
@@ -104,10 +47,8 @@ double distanceBetweenPoints(Point a, Point b)
 
     double distanceBetweenZ = pow(a.z - b.z, 2);
 
-    return std::sqrt(distanceBetweenX + distanceBetweenZ + distanceBetweenZ);
+    return sqrt(distanceBetweenX + distanceBetweenY + distanceBetweenZ);
 }
-// d(a, b);
-// a.d(b);
 
 bool AreNear(double value, double secondValue, double tolerance)
 {
